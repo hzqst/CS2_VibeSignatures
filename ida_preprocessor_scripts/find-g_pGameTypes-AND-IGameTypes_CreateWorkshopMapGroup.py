a6ab16c121f65f2fd8981f4ad72a39e5c4136c63
hzqst <113660872@qq.com>
2026-02-16 20:57:14 +0800

    Add find-g_pGameTypes-AND-IGameTypes_CreateWorkshopMapGroup

---
+++ ida_preprocessor_scripts/find-g_pGameTypes-AND-IGameTypes_CreateWorkshopMapGroup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Preprocess script for find-g_pGameTypes-AND-IGameTypes_CreateWorkshopMapGroup skill."""
+
+from ida_analyze_util import preprocess_common_skill
+
+
+TARGET_FUNCTION_NAMES = [
+    "IGameTypes_CreateWorkshopMapGroup",
+]
+TARGET_GLOBALVAR_NAMES = ["g_pGameTypes"]
+
+
+async def preprocess_skill(
+    session, skill_name, expected_outputs, old_yaml_map,
+    new_binary_dir, platform, image_base, debug=False,
+):
+    """Reuse previous gamever func_sig/gv_sig to locate targets and write YAML."""
+    return await preprocess_common_skill(
+        session=session,
+        expected_outputs=expected_outputs,
+        old_yaml_map=old_yaml_map,
+        new_binary_dir=new_binary_dir,
+        platform=platform,
+        image_base=image_base,
+        func_names=TARGET_FUNCTION_NAMES,
+        gv_names=TARGET_GLOBALVAR_NAMES,
+        debug=debug,
+    )
