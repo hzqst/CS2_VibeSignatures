8f6e572706c1ad9ab8e57d4df0685fcc5ee8b0a0
hzqst <113660872@qq.com>
2026-02-16 15:40:29 +0800

    Add find-g_pGameRules-AND-CGameRules_FindPickerEntity

--- ida_preprocessor_scripts/find-CCSGameRules_FindPickerEntity.py
+++ ida_preprocessor_scripts/find-CCSGameRules_FindPickerEntity.py
@@ -3,16 +3,24 @@
 
 from ida_analyze_util import preprocess_common_skill
 
-TARGET_FUNCTION_NAMES = [
-    "CCSGameRules_FindPickerEntity",
+INHERIT_VFUNCS = [
+    # (target_func_name, inherit_vtable_class, base_vfunc_name, generate_func_sig)
+    ("CCSGameRules_FindPickerEntity", "CCSGameRules", "CGameRules_FindPickerEntity", True),
 ]
 
-
 async def preprocess_skill(
-    session, skill_name, expected_outputs, old_yaml_map,
-    new_binary_dir, platform, image_base, debug=False,
+    session,
+    skill_name,
+    expected_outputs,
+    old_yaml_map,
+    new_binary_dir,
+    platform,
+    image_base,
+    debug=False,
 ):
-    """Reuse previous gamever func_sig to locate target function(s) and write YAML."""
+    """Resolve CCSGameRules_FindPickerEntity by their respective CCSGameRules vfunc indices."""
+    _ = skill_name
+
     return await preprocess_common_skill(
         session=session,
         expected_outputs=expected_outputs,
@@ -20,6 +28,6 @@ async def preprocess_skill(
         new_binary_dir=new_binary_dir,
         platform=platform,
         image_base=image_base,
-        func_names=TARGET_FUNCTION_NAMES,
+        inherit_vfuncs=INHERIT_VFUNCS,
         debug=debug,
     )
