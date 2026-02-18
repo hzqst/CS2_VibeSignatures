f02aba43100393a381caaa1c6c65f38948ff2d61
hzqst <113660872@qq.com>
2026-02-18 23:34:20 +0800

    Add IGameSystem_ClientPreUpdate.windows.yaml

---
+++ ida_preprocessor_scripts/find-IGameSystem_vtable.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Preprocess script for find-IGameSystem_vtable skill."""
+
+from ida_analyze_util import preprocess_common_skill
+
+TARGET_CLASS_NAMES = ["IGameSystem"]
+
+
+async def preprocess_skill(
+    session, skill_name, expected_outputs, old_yaml_map,
+    new_binary_dir, platform, image_base, debug=False,
+):
+    """Generate IGameSystem vtable YAML by class-name lookup via MCP."""
+    return await preprocess_common_skill(
+        session=session,
+        expected_outputs=expected_outputs,
+        vtable_class_names=TARGET_CLASS_NAMES,
+        platform=platform,
+        image_base=image_base,
+        debug=debug,
+    )
