aeeabdd93fcc298cd6913d80fb0a91f196e07860
hzqst <113660872@qq.com>
2026-02-19 19:10:51 +0800

    Fix #66 Fix #67 Fix #68

---
+++ ida_preprocessor_scripts/find-IGameSystem_ClientPostRender.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Preprocess script for find-IGameSystem_ClientPostRender skill."""
+
+from ida_analyze_util import preprocess_common_skill
+
+INHERIT_VFUNCS = [
+    # (target_func_name, inherit_vtable_class, base_vfunc_name, generate_func_sig)
+    ("IGameSystem_ClientPostRender", "IGameSystem", "CBuildCubemapsGameSystem_ClientPostRender", False),
+]
+
+async def preprocess_skill(
+    session,
+    skill_name,
+    expected_outputs,
+    old_yaml_map,
+    new_binary_dir,
+    platform,
+    image_base,
+    debug=False,
+):
+    """Resolve IGameSystem_ClientPostRender by their respective CBuildCubemapsGameSystem_ClientPostRender vfunc indices."""
+    _ = skill_name
+
+    return await preprocess_common_skill(
+        session=session,
+        expected_outputs=expected_outputs,
+        old_yaml_map=old_yaml_map,
+        new_binary_dir=new_binary_dir,
+        platform=platform,
+        image_base=image_base,
+        inherit_vfuncs=INHERIT_VFUNCS,
+        debug=debug,
+    )
