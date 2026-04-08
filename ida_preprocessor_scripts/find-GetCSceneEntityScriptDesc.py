f1a28f0eaee8e8f21dabbd795884a28f1097c723
hzqst <113660872@qq.com>
2026-04-08 09:49:36 +0800

    Complete https://github.com/HLND2T/CS2_VibeSignatures/issues/203

---
+++ ida_preprocessor_scripts/find-GetCSceneEntityScriptDesc.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Preprocess script for find-GetCSceneEntityScriptDesc skill."""
+
+from ida_analyze_util import preprocess_common_skill
+
+TARGET_FUNCTION_NAMES = [
+    "GetCSceneEntityScriptDesc",
+]
+
+FUNC_XREFS = [
+    # (func_name, xref_strings_list, xref_funcs_list, exclude_funcs_list)
+    (
+        "GetCSceneEntityScriptDesc",
+        [
+            "Choreographed scene which controls animation and/or dialog on one or more actors.",
+        ],
+        [],
+        [],
+    ),
+]
+
+FUNC_VTABLE_RELATIONS = [
+    # (func_name, vtable_class, generate_vfunc_offset)
+]
+
+async def preprocess_skill(
+    session, skill_name, expected_outputs, old_yaml_map,
+    new_binary_dir, platform, image_base, debug=False,
+):
+    """Reuse previous gamever func_sig to locate target function(s) and write YAML."""
+    return await preprocess_common_skill(
+        session=session,
+        expected_outputs=expected_outputs,
+        old_yaml_map=old_yaml_map,
+        new_binary_dir=new_binary_dir,
+        platform=platform,
+        image_base=image_base,
+        func_names=TARGET_FUNCTION_NAMES,
+        func_xrefs=FUNC_XREFS,
+        func_vtable_relations=FUNC_VTABLE_RELATIONS,
+        debug=debug,
+    )
